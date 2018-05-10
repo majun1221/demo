ce455f553befab2b1cecf03b725129a254ae0ffe
马俊 <836046371@qq.com>
2018-05-10 11:31:08 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 陕西耳垂网络科技有限公司 on 2018/5/10.
+//  Copyright © 2018年 陕西耳垂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
